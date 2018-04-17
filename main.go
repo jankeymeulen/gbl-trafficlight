80812cc220078dff58ce59639eea61fee020af01
Jan <jan@keymeulen.com>
2018-04-18 00:30:38 +0200

    bot working

--- main.go
+++ main.go
@@ -9,30 +9,48 @@ import (
 	//"google.golang.org/appengine/log"
 )
 
-type Pattern struct {
-	Name 	string
-	R	int
-	G	int
-	B	int
-	Datestamp	time.Time
-}
-
-type DynamiteSender struct {
-	name	string
-	email	string
-}
-
-type DynamiteMessage struct {
-	text	string
-	sender	DynamiteSender
+type DynamiteReply struct {
+	Text	string	`json:"text"`
 }
 
 type DynamiteCall struct {
-	calltype	string `json:"type"`
-	message 	DynamiteMessage
+	Type      string    `json:"type"`
+	Token     string    `json:"token"`
+	EventTime time.Time `json:"event_time"`
+	Space     struct {
+		Name        string `json:"name"`
+		DisplayName string `json:"displayName"`
+		Type        string `json:"type"`
+	} `json:"space"`
+	Message struct {
+		Name   string `json:"name"`
+		Sender struct {
+			Name        string `json:"name"`
+			DisplayName string `json:"displayName"`
+			AvatarURL   string `json:"avatarUrl"`
+			Email       string `json:"email"`
+		} `json:"sender"`
+		CreateTime time.Time `json:"createTime"`
+		Text       string    `json:"text"`
+		Thread     struct {
+			Name string `json:"name"`
+		} `json:"thread"`
+	} `json:"message"`
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+type Effect struct {
+	Name	string
+	Colour struct {
+		R int8
+		G int8
+		B int8
+	}
+	UpdateTime	time.Time
+}
+
+
+
+func dynamiteHandler(w http.ResponseWriter, r *http.Request) {
 
 	var call DynamiteCall
 
@@ -46,10 +64,31 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w,"Error parsing request: " + err.Error(),400)
 			return
 		}	
-	fmt.Fprintln(w,"Hello world! "+call.message)
+	
+	var reply DynamiteReply
+
+	if call.Type == "ADDED_TO_SPACE" {
+			reply.Text = "Thanks for adding me!"
+		}
+	if call.Type == "MESSAGE" {
+			var response = handleMessage(call)
+			reply.Text = "Message received! " + response
+		}
+	w.Header().Set("Content-Type", "application/json")
+    	w.WriteHeader(http.StatusCreated)
+    	json.NewEncoder(w).Encode(reply)
+}
+
+func handleMessage (call DynamiteCall) (string) {
+	return call.Message.Text
+}
+
+func effectHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w,"Hello world!")
 }
 
 func main() {
-	http.HandleFunc("/push",indexHandler)
+	http.HandleFunc("/dynamite",dynamiteHandler)
+	http.HandleFunc("/effect",effectHandler)
 	appengine.Main()
 }
