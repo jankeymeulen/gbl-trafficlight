4e07991ba80c3a9fc8c14ec27211957e5b1ec338
Jan <jan@keymeulen.com>
2018-04-22 23:36:45 +0200

    added colour parsing

--- main.go
+++ main.go
@@ -7,8 +7,15 @@ import (
 	"time"
 	"google.golang.org/appengine/datastore"
 	"regexp"
+	"strings"
+	"strconv"
 	//"google.golang.org/appengine/log"
 )
+
+
+const effectre = "(SOLID)|(BLINK)"
+const rgbre = "[[:digit:]]{1,3} [[:digit:]]{1,3} [[:digit:]]{1,3}"
+const colourre = "(aliceblue)|(antiquewhite)|(aqua)|(aquamarine)|(azure)|(beige)|(bisque)|(black)|(blanchedalmond)|(blue)|(blueviolet)|(brown)|(burlywood)|(cadetblue)|(chartreuse)|(chocolate)|(coral)|(cornflowerblue)|(cornsilk)|(crimson)|(cyan)|(darkblue)|(darkcyan)|(darkgoldenrod)|(darkgray)|(darkgreen)|(darkgrey)|(darkkhaki)|(darkmagenta)|(darkolivegreen)|(darkorange)|(darkorchid)|(darkred)|(darksalmon)|(darkseagreen)|(darkslateblue)|(darkslategray)|(darkslategrey)|(darkturquoise)|(darkviolet)|(deeppink)|(deepskyblue)|(dimgray)|(dimgrey)|(dodgerblue)|(firebrick)|(floralwhite)|(forestgreen)|(fuchsia)|(gainsboro)|(ghostwhite)|(gold)|(goldenrod)|(gray)|(grey)|(green)|(greenyellow)|(honeydew)|(hotpink)|(indianred)|(indigo)|(ivory)|(khaki)|(lavender)|(lavenderblush)|(lawngreen)|(lemonchiffon)|(lightblue)|(lightcoral)|(lightcyan)|(lightgoldenrodyellow)|(lightgray)|(lightgreen)|(lightgrey)|(lightpink)|(lightsalmon)|(lightseagreen)|(lightskyblue)|(lightslategray)|(lightslategrey)|(lightsteelblue)|(lightyellow)|(lime)|(limegreen)|(linen)|(magenta)|(maroon)|(mediumaquamarine)|(mediumblue)|(mediumorchid)|(mediumpurple)|(mediumseagreen)|(mediumslateblue)|(mediumspringgreen)|(mediumturquoise)|(mediumvioletred)|(midnightblue)|(mintcream)|(mistyrose)|(moccasin)|(navajowhite)|(navy)|(oldlace)|(olive)|(olivedrab)|(orange)|(orangered)|(orchid)|(palegoldenrod)|(palegreen)|(paleturquoise)|(palevioletred)|(papayawhip)|(peachpuff)|(peru)|(pink)|(plum)|(powderblue)|(purple)|(red)|(rosybrown)|(royalblue)|(saddlebrown)|(salmon)|(sandybrown)|(seagreen)|(seashell)|(sienna)|(silver)|(skyblue)|(slateblue)|(slategray)|(slategrey)|(snow)|(springgreen)|(steelblue)|(tan)|(teal)|(thistle)|(tomato)|(turquoise)|(violet)|(wheat)|(white)|(whitesmoke)|(yellow)|(yellowgreen)"
 
 type Effect struct {
 	Name	string
@@ -82,7 +89,7 @@ func dynamiteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleMessage (call DynamiteCall) (string) {
-	re := regexp.MustCompile("^((SOLID)|(BLINK)) [[:digit:]]{1,3} [[:digit:]]{1,3} [[:digit:]]{1,3}$")
+	re := regexp.MustCompile("(?i)^("+effectre+") (("+rgbre+")|("+colourre+"))$")
 	if re.MatchString(call.Message.Text) {
 		return "Valid command: " + call.Message.Text
 	} else {
@@ -92,16 +99,34 @@ func handleMessage (call DynamiteCall) (string) {
 
 func parseEffect (message string) (Effect) {
 	var e Effect
-	e.Name = message
+	re := regexp.MustCompile(effectre)	
+	e.Name = strings.ToUpper(re.FindString(message))
+	re = regexp.MustCompile(colourre)
+	var c Colour
+	if  re.MatchString(colourre) {
+		c = getColourMap[re.FindString(message)]
+	} else {
+		re := regexp.MustCompile(rgbre)
+		rgb := strings.Split(re.FindString(message)," ")
+		r,_ := strconv.Atoi(rgb[0])
+		g,_ := strconv.Atoi(rgb[1])
+		b,_ := strconv.Atoi(rgb[2])
+		c = Colour{"custom",uint8(r),uint8(g),uint8(b)}
+		e.Colour = c
+	}
 	return e
 }
 
 func effectHandler(w http.ResponseWriter, r *http.Request) {
 		ctx := appengine.NewContext(r)
-		q := datastore.NewQuery("DynamiteCall").Order("Message.CreateTime").Limit(1)
+		q := datastore.NewQuery("DynamiteCall").Order("-Message.CreateTime").Limit(1)
 		var calls []DynamiteCall
 		if _, err := q.GetAll(ctx, &calls); err != nil {
 			http.Error(w,"Error retrieving from datastore: " + err.Error(), 400)
+			return
+		}
+		if len(calls) !=1 {
+			http.Error(w,"No message found in the datastore.",400)
 			return
 		}
 		var e Effect = parseEffect(calls[0].Message.Text)
